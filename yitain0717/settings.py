5a2f8e9ed8eaee20d70463e685c56c61ef01902a
11056030 <11056030@ntub.edu.tw>
2023-09-27 15:54:20 +0800

    呱呱好笨

--- yitain0717/settings.py
+++ yitain0717/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i$u^rfeiy3l7(j$5+dchk8-u+$cjl)iv$r80dptks+i&k=i8+l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
+    'mysite',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hant'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Taipei'
 
 USE_I18N = True
 
