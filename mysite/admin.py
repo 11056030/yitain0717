25feadcf7fd32137386f0b853335a0f97b4c4727
11056030 <11056030@ntub.edu.tw>
2023-10-04 15:52:01 +0800

    新增post物件跟管理介面

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from mysite.models import Post
 
+class PostAdmin(admin.ModelAdmin):    #管理者介面可以多看到這幾個
+    list_display=('title','slug','pub_date')
+    
 # Register your models here.
+
+admin.site.register(Post,PostAdmin)
--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title=models.CharField(max_length=200) 
+    slug=models.CharField(max_length=200)
+    body=models.TextField()
+    pub_date=models.DateTimeField(auto_now_add=True)  #日期 時間(自動取得)
+    
+    class Meta:
+        ordering=('-pub_date', ) #,強制新增combo
+        
+    def __str__(self) -> str:
+        return self.title
